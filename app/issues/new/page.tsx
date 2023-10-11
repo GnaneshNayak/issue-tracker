46385b03fd959e0a3cd75505f8c643c95872cd97
GnaneshNayak <mr.gnaneshnayak@gmail.com>
2023-10-11 17:11:46 +0530

    Add MDE mark down editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { Button, TextArea, TextField } from '@radix-ui/themes';
 import React from 'react';
+import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
 
 type Props = {};
 
@@ -10,7 +12,7 @@ const NewIssue = (props: Props) => {
       <TextField.Root>
         <TextField.Input placeholder="Title" />
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button> Submit New Issue</Button>
     </div>
   );
