9413b4b1560aa38ab59da952e32ed4f83a2445ba
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2023-10-13 20:47:12 +0530

    checking dynamic function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,22 +1,26 @@
 'use client';
-import { Button, Callout, Text, TextArea, TextField } from '@radix-ui/themes';
-import React, { useState } from 'react';
-import SimpleMDE from 'react-simplemde-editor';
-import 'easymde/dist/easymde.min.css';
-import { Controller, useForm } from 'react-hook-form';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
-import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
-import delay from 'delay';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import axios from 'axios';
+import 'easymde/dist/easymde.min.css';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import SimpleMDE from 'react-simplemde-editor';
+import { z } from 'zod';
 
 // interface IssueForm {
 //   title: string;
 //   description: string;
 // }
+
+// const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+//   ssr: false,
+// });
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
