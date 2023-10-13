d31e2dde3afbeb2a17f4e41ce899e190a7d4e26b
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2023-10-13 16:31:01 +0530

    add loading skelton

---
+++ app/issues/IssueAction.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
+
+type Props = {};
+
+const IssueAction = (props: Props) => {
+  return (
+    <div className="mb-5">
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default IssueAction;
--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,18 +1,16 @@
-import prisma from '@/prisma/client';
-import { Button, Table } from '@radix-ui/themes';
-import Link from 'next/link';
+import { Table } from '@radix-ui/themes';
 import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import IssueStatusBadge from '../components/issueStatusBadge';
-import delay from 'delay';
 import IssueAction from './IssueAction';
 
 type Props = {};
 
-const IsuuesPage = async (props: Props) => {
-  await delay(2000);
-  const issues = await prisma.issue.findMany();
+const loading = (props: Props) => {
+  const issues = [1, 2, 3, 4, 5];
   return (
-    <div>
+    <>
       <IssueAction />
       <Table.Root variant="surface">
         <Table.Header>
@@ -28,25 +26,25 @@ const IsuuesPage = async (props: Props) => {
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.Cell>
-                {issue.title}
+                <Skeleton />
                 <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
+                  <Skeleton />
                 </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell ">
-                <IssueStatusBadge status={issue.status} />
+                <Skeleton />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell ">
-                {issue.createdAt.toDateString()}
+                <Skeleton />
               </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
       </Table.Root>
-    </div>
+    </>
   );
 };
 
-export default IsuuesPage;
+export default loading;
