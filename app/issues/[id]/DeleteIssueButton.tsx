f0cc79e2c158fd2ce22d6e1bbca49d17970fe633
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2023-10-15 18:26:34 +0530

    show a confirmation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,41 @@
+'use client';
 import { Issue } from '@prisma/client';
-import { Button } from '@radix-ui/themes';
+
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 
 type Props = {
   issueId: number;
 };
 
 const DeleteIssueButton = ({ issueId }: Props) => {
-  return <Button color="red">Delete Issue</Button>;
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure you want to delete this issue? This action cannot be
+            undone.
+          </AlertDialog.Description>
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red">
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
+  );
 };
 
 export default DeleteIssueButton;
