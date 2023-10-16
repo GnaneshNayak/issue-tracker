717b31e6b3cfa052a56e2359cfda6849f1717fc7
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2023-10-16 16:49:53 +0530

    add a loading skeleton

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -13,6 +13,7 @@ import {
   Flex,
   Text,
 } from '@radix-ui/themes';
+import { Skeleton } from '@/app/components';
 
 type Props = {};
 
@@ -69,7 +70,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === 'loading') return null;
+  if (status === 'loading') return <Skeleton width="3rem" />;
 
   if (status === 'unauthenticated')
     return (
