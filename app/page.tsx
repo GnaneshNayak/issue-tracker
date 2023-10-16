baf516c017bf88146b7fd5a27f4a4ce5c75b3f91
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2023-10-16 15:25:34 +0530

    Add the prisma adapter

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <div>
       <h1>hello</h1>
+      <Link href="/api/auth/signin">login</Link>
+      <Link href="/api/auth/signout">logout</Link>
+      {/* <Link>login</Link> */}
     </div>
   );
 }
