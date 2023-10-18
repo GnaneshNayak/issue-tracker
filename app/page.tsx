700831aa0e75942c0b153ad89525716c7ef88868
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2023-10-18 21:07:20 +0530

    home page made dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import Pagination from './components/Pagination';
-import LatestIssues from './LatestIssues';
-import IssueSummary from './issueSummary';
 import prisma from '@/prisma/client';
-import IssueChart from './IssueChart';
 import { Flex, Grid } from '@radix-ui/themes';
 import { Metadata } from 'next';
+import IssueChart from './IssueChart';
+import LatestIssues from './LatestIssues';
+import IssueSummary from './issueSummary';
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: 'OPEN' } });
@@ -24,6 +21,8 @@ export default async function Home() {
     </Grid>
   );
 }
+
+export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
   title: 'Issue Tracker - Dashboard',
