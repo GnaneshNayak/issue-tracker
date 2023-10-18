36856f4cdc48c80bf6590987c699a4ba7f4d807d
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2023-10-18 16:11:53 +0530

    build latest issue table component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Pagination from './components/Pagination';
+import LatestIssues from './LatestIssues';
 
 export default function Home({
   searchParams,
@@ -8,16 +9,8 @@ export default function Home({
   searchParams: { page: string };
 }) {
   return (
-    <div>
-      <h1>hello</h1>
-      <Link href="/api/auth/signin">login</Link>
-      <Link href="/api/auth/signout">logout</Link>
-      <Pagination
-        itemCount={100}
-        pageSize={10}
-        currentPage={parseInt(searchParams.page)}
-      />
-      {/* <Link>login</Link> */}
-    </div>
+    <>
+      <LatestIssues />
+    </>
   );
 }
